d0fbe6ea06241118f30d6c7565dd9d6c2bcb40f9
Emilio Dominguez <edominguezsa@eservicios.indra.es>
2024-01-31 13:50:22 +0100

    Initial Commit

---
+++ src/main/java/com/testemilio/pricelistservice/PriceListServiceApplication.java
@@ -0,0 +1,13 @@
+package com.testemilio.pricelistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceListServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceListServiceApplication.class, args);
+    }
+
+}
